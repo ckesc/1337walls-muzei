aceb3831512d6ed12c49b870c0cbb4d3dbb41212
CkEsc <iamckesc@gmail.com>
2015-02-01 04:18:11 +0300

    Lollipoping settings =)

--- app/src/main/java/de/zigamorph/w8alls/SettingsActivity.java
+++ app/src/main/java/de/zigamorph/w8alls/SettingsActivity.java
@@ -1,18 +1,18 @@
 package de.zigamorph.w8alls;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
 
 import java.util.Calendar;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends ActionBarActivity {
 
     private SharedPreferences prefs;
     private boolean pref_cyclemode_start;
@@ -22,7 +22,13 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+        setContentView(R.layout.activity_preferences);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().
+                    replace(android.R.id.content, new PrefsFragment())
+                    .commit();
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         this.pref_cyclemode_start = prefs.getBoolean(getString(R.string.pref_cyclemode_key), true);
